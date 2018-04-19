14b0ce00d5c6158e630d3da65e29091c9d8dda54
Iris Yu <iyucomms@gmail.com>
2018-04-18 17:17:48 -0700

    Add basic GeoJSON density fill layer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import MapLayer from './MapLayer';
+import DensityMap from './DensityMap.js';
 import './App.css';
+const sampleData = require('./sampleData');
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-          <MapLayer />
+          <DensityMap disasterData={sampleData} />
       </div>
     )
   }
