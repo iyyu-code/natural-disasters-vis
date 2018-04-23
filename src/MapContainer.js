45ef604e630c88b056625dadf4609b309e0f3ef2
Iris Yu <iyucomms@gmail.com>
2018-04-23 12:11:35 -0700

    Add disaster filter dropdown UI and logic

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -4,42 +4,70 @@ import { initialConfig, interactiveConfig } from './dataConfig';
 import { sampleData } from './sampleData.js';
 const axios = require('axios');
 
-export default class MapContainer extends React.Component {
+export default class MapContainer extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      sampleData: sampleData
+      disasterData: [],
+      interactiveConfig: interactiveConfig,
+      shouldUseSampleData: false
     }
+    this.loadData = this.loadData.bind(this);
+  }
+  
+  loadData = (config, initial = false) => {
+    const context = this;
+    axios(config)
+    .then(response => {
+      if (!initial) {
+        context.setState({ disasterData: response.data });
+      } else if (response.status !== 200 && response.data.count < 1) {
+        context.setState({disasterData: sampleData, shouldUseSampleData: true});
+      }
+    })
+    .catch(err => {
+        console.log(`ERROR ${err}`);
+        context.setState({disasterData: sampleData, shouldUseSampleData: true});
+      });
   }
 
   componentDidMount() {
-    axios(initialConfig)
-      .then(response => {
-        this.setState({disasterData :response.data});
-      })
-      .catch(err => {
-        console.log('Error: ' + err);
-      });
+    this.loadData(initialConfig, true);
   }
 
-  componentWillReceiveProps(nextProps) {
-    interactiveConfig.data = nextProps.interactiveQuery;
-    axios(interactiveConfig)
-      .then(response => {
-        this.setState({ disasterData: response.data });
-      })
-      .catch(err => {
-        console.log('Error: ' + err);
-      });
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.disasterSelections !== prevProps.disasterSelections || this.props.years !== prevProps.years) {
+      if (this.props.disasterSelections[0] === "") {
+        this.setState({disasterData: []});
+      } else {
+        axios(interactiveConfig)
+          .then(response => {
+            this.setState({ disasterData: response.data });
+          })
+          .catch(err => {
+            console.log(`ERROR ${err}`);
+          });
+      }
+    }
   }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.disasterSelections.length && nextProps.years) {
+      interactiveConfig.data = nextProps.interactiveQuery;
+        return { interactiveConfig };
+    } else {
+      return null;
+    }
+  }
   
   render() {
-    return this.state.disasterData ? <div className="MapContainer">
-        <DensityMap disasterData={this.state.disasterData} />
-      </div> : <div className="MapContainer">
-        <DensityMap disasterData={this.state.sampleData} />
-      </div>;
+    return (
+      <div className="map-container">
+        <DensityMap 
+          disasterData={this.state.disasterData} 
+          shouldUseSampleData={this.state.shouldUseSampleData}/>
+      </div>
+      )
   }
 }
