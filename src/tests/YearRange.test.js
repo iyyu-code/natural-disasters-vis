44ba0bd5ba956984b6421668cb8d4265db5520b3
Iris Yu <iyyu@users.noreply.github.com>
2018-04-23 17:20:32 -0700

    Add snapshot tests (#8)

---
+++ src/tests/YearRange.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YearRange from '../YearRange';
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<YearRange />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders correctly', () => {
+  const range = renderer.create(<YearRange />).toJSON();
+  expect(range).toMatchSnapshot();
+});
